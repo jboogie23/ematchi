6e60785e17ad01f02ef3d83f247c4fafe52af934
Ross <ross.h.sanderson@gmail.com>
2024-02-08 22:31:46 +1100

    finished grid and initial styling

---
+++ src/routes/Game.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
+<div class="game">
+	<div class="info"></div>
+
+	<div class="grid-container">
+		<Grid />
+	</div>
+
+	<div class="info"></div>
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		font-size: min(1vmin, 0.5rem);
+	}
+
+	.info {
+		width: 80em;
+		height: 10em;
+		background: purple;
+	}
+
+	.grid-container {
+		width: 80em;
+		height: 80em;
+		background: teal;
+	}
+</style>
---
+++ src/routes/Square.svelte
@@ -0,0 +1,7 @@
+<div class="square"></div>
+
+<style>
+	.square {
+		background: yellow;
+	}
+</style>
