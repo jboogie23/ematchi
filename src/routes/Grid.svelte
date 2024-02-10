68d785adb373a9dced6355a33940540f98a7c3d5
Ross <ross.h.sanderson@gmail.com>
2024-02-10 18:35:44 +1100

    game is working. needs better presentation. needs working pause button.

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,10 +1,44 @@
-<script>
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import Square from './Square.svelte';
+
+	export let grid: string[];
+	export let found: string[];
+
+	const dispatch = createEventDispatcher();
+
+	let a = -1;
+	let b = -1;
+	let reset_timeout: number;
 </script>
 
 <div class="grid">
-	{#each Array(16) as _}
-		<Square />
+	{#each grid as emoji, i}
+		<Square
+			{emoji}
+			on:click={() => {
+				clearTimeout(reset_timeout);
+				if (a === -1 && b === -1) {
+					a = i;
+				} else if (b === -1) {
+					b = i;
+					if (grid[a] === grid[b]) {
+						dispatch('found', {
+							emoji
+						});
+					} else {
+						setTimeout(() => {
+							a = b = -1;
+						}, 1000);
+					}
+				} else {
+					b = -1;
+					a = i;
+				}
+			}}
+			selected={a === i || b === i}
+			found={found.includes(emoji)}
+		/>
 	{/each}
 </div>
 
@@ -15,5 +49,6 @@
 		grid-template-rows: repeat(4, 1fr);
 		grid-gap: 0.5em;
 		height: 100%;
+		perspective: 100vw;
 	}
 </style>
